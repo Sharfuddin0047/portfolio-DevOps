a173843d9fdc892f96a1ce8a30d147eb0bd2799c
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-03-05 21:34:08 +0530

    print exactly once

---
+++ script.js
@@ -0,0 +1,42 @@
+// typing animation
+
+const text = [
+"Software Developer",
+"Backend Engineer",
+"DevOps Enthusiast"
+]
+
+let count = 0
+let index = 0
+let currentText = ""
+let letter = ""
+
+function type(){
+
+if(count === text.length){
+count = 0
+}
+
+currentText = text[count]
+letter = currentText.slice(0,++index)
+
+document.querySelector(".typing").textContent = letter
+
+if(letter.length === currentText.length){
+count++
+index = 0
+}
+
+setTimeout(type,120)
+
+}
+
+type()
+
+
+
+function showEmail(){
+
+document.getElementById("email").innerText="your-email@example.com"
+
+}
